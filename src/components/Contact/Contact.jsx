460f14e0a5e7cd1afeb0ab498a1f47cf99e27bca
kspayne93 <kspayne93@gmail.com>
2019-03-03 13:35:14 -0700

    changed to functional components since there is no state

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,37 +3,35 @@ import { Link } from 'react-router-dom';
 import './Contact.scss';
 
 
-export default class Contact extends Component {
-   render() {
-      return (
-         <div className='page contact-page-main'>
-            <Link to='/' className='link'>
-               <i className="fas fa-undo class back-button"></i>
-            </Link>
-            <h1 style={{ marginBottom: '1rem' }} className='contact-title'>
-               Contact:
+export default function Contact() {
+   return (
+      <div className='page contact-page-main'>
+         <Link to='/' className='link'>
+            <i className="fas fa-undo class back-button"></i>
+         </Link>
+         <h1 style={{ marginBottom: '1rem' }} className='contact-title'>
+            Contact:
             </h1>
-            <div className='contact-container'>
-               <div className='contact-text'>
-                  <div>
-                     <i className="far fa-envelope contact-icon"></i>
-                     <p> kspayne93@gmail.com </p>
-                  </div>
-                  <div>
-                     <i class="fas fa-mobile-alt contact-icon"></i>
-                     <p> (385) 335-1329 </p>
-                  </div>
-                  <div>
-                     <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><i class="fab fa-linkedin contact-icon"></i></a>
-                     <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><p> www.linkedin.com/in/kylepayne93 </p></a>
-                  </div>
-                  <div>
-                     <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><i class="fab fa-github contact-icon"></i></a>
-                     <a href="https://www.github.com/kspayne93" target='_blank' rel='noopener noreferrer'><p> www.github.com/kspayne93 </p></a>
-                  </div>
+         <div className='contact-container'>
+            <div className='contact-text'>
+               <div>
+                  <i className="far fa-envelope contact-icon"></i>
+                  <p> kspayne93@gmail.com </p>
+               </div>
+               <div>
+                  <i class="fas fa-mobile-alt contact-icon"></i>
+                  <p> (385) 335-1329 </p>
+               </div>
+               <div>
+                  <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><i class="fab fa-linkedin contact-icon"></i></a>
+                  <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><p> www.linkedin.com/in/kylepayne93 </p></a>
+               </div>
+               <div>
+                  <a href="https://www.linkedin.com/in/kylepayne93" target='_blank' rel='noopener noreferrer'><i class="fab fa-github contact-icon"></i></a>
+                  <a href="https://www.github.com/kspayne93" target='_blank' rel='noopener noreferrer'><p> www.github.com/kspayne93 </p></a>
                </div>
             </div>
          </div>
-      )
-   }
+      </div>
+   )
 }
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,47 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.scss';
 
 
-export default class LandingPage extends Component {
-   render() {
-      return (
-         <div className='page landing-page-main'>
-            <div className='top-landing-container'>
-               <Link to='/skills' className='link'>
-                  <div className='landing-button-container skills-text'>
-                        Skills
+export default function LandingPage() {
+   return (
+      <div className='page landing-page-main'>
+         <div className='top-landing-container'>
+            <Link to='/skills' className='link'>
+               <div className='landing-button-container skills-text'>
+                  Skills
                   </div>
-               </Link>
-               <Link to='/projects' className='link'>
-                  <div className='landing-button-container projects-text'>
-                     Projects
+            </Link>
+            <Link to='/projects' className='link'>
+               <div className='landing-button-container projects-text'>
+                  Projects
                   </div>
-               </Link>
-            </div>
-            <div className='middle-landing-container'>
-               <div>
-                  <h1 className='orange-neon'>
-                     Kyle<span>__</span>Payne
+            </Link>
+         </div>
+         <div className='middle-landing-container'>
+            <div>
+               <h1 className='orange-neon'>
+                  Kyle<span>__</span>Payne
                   </h1>
-                  <h3 className='blue-neon'>
-                     Full-Stack<span>__</span>Web<span>___</span>Developer
+               <h3 className='blue-neon'>
+                  Full-Stack<span>__</span>Web<span>___</span>Developer
                   </h3>
-               </div>
-            </div>
-            <div className='bottom-landing-container'>
-               <Link to='/about' className='link'>
-                  <div className='landing-button-container about-title'>
-                        About
-                  </div>
-               </Link>
-               <Link to='/contact' className='link'>
-                  <div className='landing-button-container contact-title'>
-                        Contact
-                  </div>
-               </Link>
             </div>
          </div>
-      )
-   }
+         <div className='bottom-landing-container'>
+            <Link to='/about' className='link'>
+               <div className='landing-button-container about-title'>
+                  About
+                  </div>
+            </Link>
+            <Link to='/contact' className='link'>
+               <div className='landing-button-container contact-title'>
+                  Contact
+                  </div>
+            </Link>
+         </div>
+      </div>
+   )
 }
