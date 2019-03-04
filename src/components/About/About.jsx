4bbf7bc449cfb06468f81315a2f409061d2b56e5
kspayne93 <kspayne93@gmail.com>
2019-03-04 09:52:09 -0700

    made more mobile responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,7 +15,7 @@ export default class About extends Component {
                <div className='about-text'>
                   <p>I have always loved being challenged and learning new things. Coming from an analytics and real estate sales background, many times my options were limited in terms of creativity. I started dabbling with code and learning to program as a backup skill, and soon had a huge realization: development is my passion! </p>
 
-                  <p>I dropped everything to attended a full-time web-development bootcamp for 13 weeks to increase my knowledge and sharpen my skills, and have never looked back. I love continuing to learn and implement new technologies and make things come to life through code.</p>
+                  <p>I dropped everything to attended a full-time web-development bootcamp for 13 weeks to increase my knowledge and sharpen my skills, and have never looked back. I love continuing to learn and implement new technologies and make things come to life through code. </p>
 
                   <p>When I'm not coding, I enjoy spending time with my wife and dog. I love traveling, motorcycles, The Office, and I'm helplessly addicted to Mexican food and Yerba Mate. </p>
 
