0ce2916cd0a8aab5ecbe14486916577b15c3dff8
Kyle Payne <kspayne93@gmail.com>
2019-12-06 20:23:56 -0700

    adjusted

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ export default class About extends Component {
             <div className='about-container'>
                <div className='about-text'>
                   <p>Coming from an analytics and real estate background, many times I felt that my creative options were limited. Since I have always loved being challenged and learning new things, I started learning to program out of curiosity and interest. I quickly realized that I had a passion for software development and decided to take it seriously!</p>
-                  <p>I dropped everything to attended a full-time web-development bootcamp to kickstart my learning, and have been enjoying the journey ever since. I love being able to learn, implement, create, and share what I know with others each day.</p>
+                  <p>I dropped everything to pursue software development full-time, and have been enjoying the journey ever since. I love being able to learn, implement, create, and share what I know with others each day.</p>
                   <p>Outside of work, I enjoy spending my free time with my wife and dog. I love traveling, motorcycles, The Office, and I'm helplessly addicted to Mexican food and Yerba Mate.</p>
                </div>
             </div>
